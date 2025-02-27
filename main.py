ecf8067e5e64eafa2035e20f1f9c5d4ef2df377b
Taisei <taisei.960420@gmail.com>
2025-02-27 09:20:09 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import datetime
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+# 予約情報を管理するモデル
+class Booking(BaseModel):
+  booking_id: int
+  user_id: int
+  room_id: int
+  booked_num: int
+  start_date: datetime.datetime
+  end_date: datetime.datetime
+
+# ユーザー情報を管理するモデル
+class User(BaseModel):
+  user_id: int
+  user_name: str = Field(max_length=12)
+
+class Room(BaseModel):
+  room_id: int
+  room_name: str = Field(max_length=12)
+  capacity: int
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+  return {"message": "Success"}
+
+@app.post("/users")
+async def user(users: User):
+  return {"users": users}
+
+@app.post("/rooms")
+async def rooms(rooms: Room):
+  return {"rooms": rooms}
+
+@app.post("/bookings")
+async def bookings(bookings: Booking):
+  return {"bookings": bookings}
+
