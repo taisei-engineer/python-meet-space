21a66de1097ff9dba297f0f65380241bb1a03c6b
Taisei <taisei.960420@gmail.com>
2025-02-28 08:59:08 +0900

    会議室画面の作成

--- app.py
+++ app.py
@@ -3,25 +3,58 @@ import random
 import requests
 import json
 
-st.title("APIテスト画面（ユーザー)")
+page = st.sidebar.selectbox('Chose your page', ['users', 'rooms', 'bookings'])
 
-with st.form(key='user'):
-  user_id: int = random.randint(0, 10)
-  username: str = st.text_input('ユーザー名', max_chars=12)
-  data = {
-    'user_id': user_id,
-    'user_name': username
-  }
-  submit_button = st.form_submit_button(label='リクエスト送信')
+if page == 'users':
 
-if submit_button:
-  st.write('## 送信データ')
-  st.json(data)
-  st.write('## レスポンス結果')
-  url ='http://127.0.0.1:8000/users'
-  res =requests.post(
-    url,
-    data=json.dumps(data)
-  )
-  st.write(res.status_code)
-  st.json(res.json())
+  st.title("APIテスト画面（ユーザー)")
+
+  with st.form(key='user'):
+    user_id: int = random.randint(0, 10)
+    username: str = st.text_input('ユーザー名', max_chars=12)
+    data = {
+      'user_id': user_id,
+      'user_name': username
+    }
+    submit_button = st.form_submit_button(label='リクエスト送信')
+
+  if submit_button:
+    st.write('## 送信データ')
+    st.json(data)
+    st.write('## レスポンス結果')
+    url ='http://127.0.0.1:8000/users'
+    res =requests.post(
+      url,
+      data=json.dumps(data)
+    )
+    st.write(res.status_code)
+    st.json(res.json())
+
+elif page == 'rooms':
+  st.title("APIテスト画面（会議室)")
+  
+  with st.form(key='room'):
+    room_id: int = random.randint(0, 10)
+    room_name: str = st.text_input('会議室名', max_chars=12)
+    capacity: int = st.number_input('定員', step=1)
+    data = {
+      'room_id': room_id,
+      'room_name': room_name,
+      'capacity': capacity
+    }
+    submit_button = st.form_submit_button(label='リクエスト送信')
+
+  if submit_button:
+    st.write('## 送信データ')
+    st.json(data)
+    st.write('## レスポンス結果')
+    url ='http://127.0.0.1:8000/rooms'
+    res =requests.post(
+      url,
+      data=json.dumps(data)
+    )
+    st.write(res.status_code)
+    st.json(res.json())
+
+  
+    
