5506ab2e3b82e75a5cccb25a112ab3b5c4989365
Taisei <taisei.960420@gmail.com>
2025-03-07 09:20:03 +0900

    schemas.pyの作成

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
-import datetime
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
-
-# 予約情報を管理するモデル
-class Booking(BaseModel):
-  booking_id: int
-  user_id: int
-  room_id: int
-  booked_num: int
-  start_datetime: datetime.datetime
-  end_datetime: datetime.datetime
-
-# ユーザー情報を管理するモデル
-class User(BaseModel):
-  user_id: int
-  user_name: str = Field(max_length=12)
-
-class Room(BaseModel):
-  room_id: int
-  room_name: str = Field(max_length=12)
-  capacity: int
 
 app = FastAPI()
 
