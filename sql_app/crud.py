4c357ff51a61f13316fa1fd56b6284159d57843c
Taisei <taisei.960420@gmail.com>
2025-03-08 07:51:31 +0900

    crud.pyの作成

---
+++ sql_app/crud.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+# ユーザー一覧取得
+def get_users(db: Session, skip: int = 0, limit: int = 10):
+  return db.query(models.User).offset(skip).limit(limit).all()
+
+# 会議室一覧取得
+def get_rooms(db: Session, skip: int = 0, limit: int = 10):
+  return db.query(models.Room).offset(skip).limit(limit).all()
+
+# 予約一覧取得
+def get_bookings(db: Session, skip: int = 0, limit: int = 10):
+  return db.query(models.Booking).offset(skip).limit(limit).all()
+
